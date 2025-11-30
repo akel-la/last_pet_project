27f426c9bd4a3b172573bffeed169a50fc64668a
akel-la <a.l.e.k.s.e.y.u@mail.ru>
2025-12-01 00:32:24 +0300

    Расширения
    1) Установлены и подключены расширения:
    django-extensions,
    djangorestframework,
    django-debug-toolbar.

--- quizsite/quizsite/settings.py
+++ quizsite/quizsite/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quiz.apps.QuizConfig',
+    'django_extensions',
+    'rest_framework',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'quizsite.urls'

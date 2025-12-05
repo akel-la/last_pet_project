937caaf4e44c4a763b75b1e9d569b2cc0f90b190
akel-la <a.l.e.k.s.e.y.u@mail.ru>
2025-12-05 15:01:10 +0300

    API: Запрещено создание вопроса с status=Status.PUBLISHED.
    1) Если через API попытаться создать новый вопрос, у которого не может
       быть уже готовых связанных ответов, со статусом "Опубликованно", то
    тогда вернется HTTP ответ со статусом 400 и сообщением об ошибке, а
    вопрос не будет создан.

--- quizsite/quiz/api/v1/serializers.py
+++ quizsite/quiz/api/v1/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
-from ...models import Answers, Questions, Quizes
+from ...models import *
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answers
         fields = "__all__"
+
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Questions
--- quizsite/quiz/api/v1/views.py
+++ quizsite/quiz/api/v1/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .serializers import *
 from ...models import *
@@ -9,6 +10,28 @@ class AnswerViewSet(viewsets.ModelViewSet):
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Questions.objects.all()
     serializer_class = QuestionSerializer
+
+    def create(self, request, *args, **kwargs):
+        """Обработка ошибки - создание опубликованного
+        вопроса с не корректным сосотянием ответов."""
+        # Передача в сериализатор входных данных:
+        serializer = self.get_serializer(data=request.data)
+        # Валидация полученных данных:
+        serializer.is_valid(raise_exception=True)
+        try:
+            # Попытка сохранить объект в базу данных:
+            self.perform_create(serializer)
+            # Если сохранение успешно - то сохраняются заголовки для ответа:
+            headers = self.get_success_headers(serializer.data)
+            # Формирование ответа:
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        except ValueError as error:
+            return Response(status=status.HTTP_400_BAD_REQUEST,
+                            data={"detail": f"""Неверное значение поля {Questions._meta.get_field("status").verbose_name}: для того, чтобы поле {Questions._meta.get_field("status").verbose_name} имело значение {Status.PUBLISHED.label} нужно, чтобы у вопроса было {Questions.true_answers_count} верных ответов и {Questions.false_answers_count} не верных ответов.""",
+                                  "error_type": type(error).__name__,
+                                  "message_from_exception": str(error)
+                }
+            )
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quizes.objects.all()
     serializer_class = QuizSerializer
